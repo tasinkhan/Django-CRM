f3a2af79ba35d82ebef0071611363d1db910e3e1
Tasin <tasintsn@gmail.com>
2021-01-24 11:36:49 +0600

    signup and logout url added

--- djangocrm/urls.py
+++ djangocrm/urls.py
@@ -14,17 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from leads.views import LandingPageView
+from leads.views import LandingPageView, SignUpView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('leads/', include('leads.urls', namespace='leads')),
     path('', LandingPageView.as_view(), name="landing_page"),
     path('login/', LoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
+    path('signup/', SignUpView.as_view(), name="signup"),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
