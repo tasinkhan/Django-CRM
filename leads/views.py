790204a01a67e3f13cf363e6d733278846ba3ab0
Tasin <tasintsn@gmail.com>
2021-01-25 10:15:38 +0600

    loginrequiredmixin added lead views

--- leads/views.py
+++ leads/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.core.mail import send_mail
 from .models import User, Agent, Lead
@@ -17,17 +18,17 @@ class SignUpView(CreateView):
     def get_success_url(self):
         return reverse('login')
 
-class LeadListView(ListView):
+class LeadListView(LoginRequiredMixin, ListView):
     template_name = "leads/lead_list.html"
     queryset = Lead.objects.all()
     context_object_name = "leads"
 
-class LeadDetailView(DetailView):
+class LeadDetailView(LoginRequiredMixin, DetailView):
     template_name = "leads/lead_detail.html"
     queryset = Lead.objects.all()
     context_object_name = "lead"
 
-class LeadCreateView(CreateView):
+class LeadCreateView(LoginRequiredMixin, CreateView):
     template_name = "leads/lead_create.html"
     form_class = LeadForm
 
@@ -43,7 +44,7 @@ class LeadCreateView(CreateView):
         )
         return super(LeadCreateView, self).form_valid(form)
 
-class LeadUpdateView(UpdateView):
+class LeadUpdateView(LoginRequiredMixin, UpdateView):
     template_name = "leads/lead_update.html"
     queryset = Lead.objects.all()
     form_class = LeadForm
@@ -51,7 +52,7 @@ class LeadUpdateView(UpdateView):
     def get_success_url(self):
         return reverse('leads:lead_list')
 
-class LeadDeleteView(DeleteView):
+class LeadDeleteView(LoginRequiredMixin, DeleteView):
     template_name = "leads/lead_delete.html"
     queryset = Lead.objects.all()
 
