52b8590b54b4ea2d88e3a4c95951d2f6e45f8bd3
Tasin <tasintsn@gmail.com>
2021-01-25 10:14:46 +0600

    UserProfile model added for combining user,agent and leads

--- leads/models.py
+++ leads/models.py
@@ -5,8 +5,15 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
