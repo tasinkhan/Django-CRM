617a80e1486a32eb070f036655b9deb2437740cf
Tasin <tasintsn@gmail.com>
2021-01-31 10:45:29 +0600

    ListView, CreateView, DetailView, UpdateView, DeleteView added

--- agents/views.py
+++ agents/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect, reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from leads.models import Agent
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 from .forms import AgentModelForm
 
 # Create your views here.
@@ -23,4 +23,23 @@ class AgentCreateView(LoginRequiredMixin, CreateView):
         agent.save()
 
         return super(AgentCreateView, self).form_valid(form)
+
+class AgentDetailView(DetailView):
+    template_name = "agents/agent_detail.html"
+    queryset = Agent.objects.all()
+    context_object_name = "agent"
+
+class AgentUpdateView(UpdateView):
+    template_name = "agents/agent_update.html"
+    form_class = AgentModelForm
+    queryset = Agent.objects.all()
     
+    def get_success_url(self):
+        return reverse('agents:agent_list')
+
+class AgentDeleteView(DeleteView):
+    template_name = "agents/agent_delete.html"
+    queryset = Agent.objects.all()
+    
+    def get_success_url(self):
+        return reverse('agents:agent_list')
